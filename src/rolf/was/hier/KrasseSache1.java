08ba334bc45f69f0fff98790b8a8396249118f9b
goetz <goetz@5-6--3.Bildung.zz>
2016-05-26 15:08:33 +0200

    Voll funktionsfähig, bei Eingabe Schönheitsfehler beseitigt

---
+++ src/rolf/was/hier/KrasseSache1.java
@@ -0,0 +1,20 @@
+package rolf.was.hier;
+
+import .*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author goetz
+ */
+public class KrasseSache1 {
+    public static void main(String[] args) {
+        
+    }
+            
+}
